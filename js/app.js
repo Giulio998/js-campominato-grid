96dae838b5480ae2c668ad92b8011e2ec6863316
Giulio998 <giulio_favia@hotmail.it>
2024-03-14 09:43:07 +0100

    Refinement

--- js/app.js
+++ js/app.js
@@ -1,41 +1,32 @@
 
 function startGame() {
-    
-
-
     const grid = document.getElementById("grid");
-    grid.innerHTML = "" ;
-    let size ;
-    const difficulty = document.getElementById("difficolta").value
-    if (difficulty == "facile") {
-         size = 10
-    } else if (difficulty == "media") {
-         size = 9
-    } else {
-        size = 8
-    }
-
-    
+    grid.innerHTML = "" ; 
+    const size = parseInt(document.getElementById("difficolta").value)
     const numCells = size ** 2 ;
 
-    for (let i = 0; i < numCells; i++) {
-        const num = i + 1;
-        const cell = document.createElement("div");
-        cell.classList.add("cell") ;
-        if (size == 10) {
-            cell.classList.add("cell10")
-        } else if (size == 9) {
-            cell.classList.add("cell9")
-        } else {
-            cell.classList.add("cell8")
-        }
-
-        cell.innerHTML = num ;
-        grid.append(cell);
-        cell.addEventListener("click", function() {
-            boxToggle(num, cell)           
-        });
-    }   
+    function gridGenerator() {
+        for (let i = 0; i < numCells; i++) {
+            const num = i + 1;
+            const cell = document.createElement("div");
+            cell.classList.add("cell") ;
+            if (size == 10) {
+                cell.classList.add("cell10")
+            } else if (size == 9) {
+                cell.classList.add("cell9")
+            } else {
+                cell.classList.add("cell8")
+            }
+    
+            cell.innerHTML = num ;
+            grid.append(cell);
+            cell.addEventListener("click", function() {
+                boxToggle(num, cell)           
+            });
+        }   
+    }
+    
+    gridGenerator()
 
     function boxToggle(num, cell) {
         console.log(num);
